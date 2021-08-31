1865acc99aa70afaf9d2fd291ddd1080d171c7e4
stephen-wu-chaohui <stephen.wu.chaohui@gmail.com>
2021-08-31 21:39:47 +1200

    Remove CustomController.cs

--- Bread.API/Configurations/SwaggerExtensions.cs
+++ Bread.API/Configurations/SwaggerExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
